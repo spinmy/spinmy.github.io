059a229324646af4c7017f82408674c067199548
ghost <sandbox@kali.org>
2023-08-26 21:39:38 +1000

    Initial commit

--- js/beta.js
+++ js/beta.js
@@ -86,7 +86,7 @@ class playGame extends Phaser.Scene {
                 onComplete: function (tween) {
                     document.getElementById("pyro").style.display = "block";
                     var prize = gameOptions.slicePrizes[targetSlice];
-                    this.prizeText.setText(prize);
+                    this.prizeText.setText("$ " + prize + " JACKPOT!!!");
                     this.canSpin = true;
                 }
             });
